8537e06a6efa1c3fc02a2e3c434f61f68766d0d5
Akexorcist <akexorcist@gmail.com>
2023-12-28 22:54:48 +0700

    Update

--- precache-manifest.7dc6c27128060afa204205e97607d36b.js
+++ precache-manifest.7dc6c27128060afa204205e97607d36b.js
@@ -1,94 +1,94 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "37661649c527f7834109de3dc80b03ff",
-    "url": "/deep-link-generator/index.html"
+    revision: "37661649c527f7834109de3dc80b03ff",
+    url: "/index.html",
   },
   {
-    "revision": "85a26c90c2406006db89",
-    "url": "/deep-link-generator/static/css/2.5e57fced.chunk.css"
+    revision: "85a26c90c2406006db89",
+    url: "/static/css/2.5e57fced.chunk.css",
   },
   {
-    "revision": "c47e4094d2cdae40e13d",
-    "url": "/deep-link-generator/static/css/main.65a4c388.chunk.css"
+    revision: "c47e4094d2cdae40e13d",
+    url: "/static/css/main.65a4c388.chunk.css",
   },
   {
-    "revision": "85a26c90c2406006db89",
-    "url": "/deep-link-generator/static/js/2.b054b934.chunk.js"
+    revision: "85a26c90c2406006db89",
+    url: "/static/js/2.b054b934.chunk.js",
   },
   {
-    "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/deep-link-generator/static/js/2.b054b934.chunk.js.LICENSE.txt"
+    revision: "0749163b59fbee32225059cb60c18af6",
+    url: "/static/js/2.b054b934.chunk.js.LICENSE.txt",
   },
   {
-    "revision": "c47e4094d2cdae40e13d",
-    "url": "/deep-link-generator/static/js/main.33ab97be.chunk.js"
+    revision: "c47e4094d2cdae40e13d",
+    url: "/static/js/main.33ab97be.chunk.js",
   },
   {
-    "revision": "d89bce869314f645d8f1",
-    "url": "/deep-link-generator/static/js/runtime-main.fce09b18.js"
+    revision: "d89bce869314f645d8f1",
+    url: "/static/js/runtime-main.fce09b18.js",
   },
   {
-    "revision": "c762d850e2e8d0e29047608715196736",
-    "url": "/deep-link-generator/static/media/roboto-all-400-italic.c762d850.woff"
+    revision: "c762d850e2e8d0e29047608715196736",
+    url: "/static/media/roboto-all-400-italic.c762d850.woff",
   },
   {
-    "revision": "a91ad097d24828af724d4fee36a063ed",
-    "url": "/deep-link-generator/static/media/roboto-all-400-normal.a91ad097.woff"
+    revision: "a91ad097d24828af724d4fee36a063ed",
+    url: "/static/media/roboto-all-400-normal.a91ad097.woff",
   },
   {
-    "revision": "8266321ab43353bcd147ad67600d165c",
-    "url": "/deep-link-generator/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+    revision: "8266321ab43353bcd147ad67600d165c",
+    url: "/static/media/roboto-cyrillic-400-italic.8266321a.woff2",
   },
   {
-    "revision": "8bb64952764a884d67019b3486296ab9",
-    "url": "/deep-link-generator/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+    revision: "8bb64952764a884d67019b3486296ab9",
+    url: "/static/media/roboto-cyrillic-400-normal.8bb64952.woff2",
   },
   {
-    "revision": "b3e580d221a4722c959155878ab94210",
-    "url": "/deep-link-generator/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+    revision: "b3e580d221a4722c959155878ab94210",
+    url: "/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2",
   },
   {
-    "revision": "4743c758a952f2bd4a35d4e42afc002b",
-    "url": "/deep-link-generator/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+    revision: "4743c758a952f2bd4a35d4e42afc002b",
+    url: "/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2",
   },
   {
-    "revision": "469a78405c3ae073ba769321fa0584f3",
-    "url": "/deep-link-generator/static/media/roboto-greek-400-italic.469a7840.woff2"
+    revision: "469a78405c3ae073ba769321fa0584f3",
+    url: "/static/media/roboto-greek-400-italic.469a7840.woff2",
   },
   {
-    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
-    "url": "/deep-link-generator/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+    revision: "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    url: "/static/media/roboto-greek-400-normal.c1e9793c.woff2",
   },
   {
-    "revision": "801b64f75ae26dab9c36f00629ebdad0",
-    "url": "/deep-link-generator/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+    revision: "801b64f75ae26dab9c36f00629ebdad0",
+    url: "/static/media/roboto-greek-ext-400-italic.801b64f7.woff2",
   },
   {
-    "revision": "182ee6a4872ca8fa78048951b1561a5c",
-    "url": "/deep-link-generator/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+    revision: "182ee6a4872ca8fa78048951b1561a5c",
+    url: "/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2",
   },
   {
-    "revision": "51521a2a8da71e50d871ac6fd2187e87",
-    "url": "/deep-link-generator/static/media/roboto-latin-400-italic.51521a2a.woff2"
+    revision: "51521a2a8da71e50d871ac6fd2187e87",
+    url: "/static/media/roboto-latin-400-italic.51521a2a.woff2",
   },
   {
-    "revision": "479970ffb74f2117317f9d24d9e317fe",
-    "url": "/deep-link-generator/static/media/roboto-latin-400-normal.479970ff.woff2"
+    revision: "479970ffb74f2117317f9d24d9e317fe",
+    url: "/static/media/roboto-latin-400-normal.479970ff.woff2",
   },
   {
-    "revision": "dc3871f486e189164db4b98968330bc4",
-    "url": "/deep-link-generator/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+    revision: "dc3871f486e189164db4b98968330bc4",
+    url: "/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2",
   },
   {
-    "revision": "455200cb007fe1212c668721d827c691",
-    "url": "/deep-link-generator/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+    revision: "455200cb007fe1212c668721d827c691",
+    url: "/static/media/roboto-latin-ext-400-normal.455200cb.woff2",
   },
   {
-    "revision": "9780bde87bceec579eaf16bddad47615",
-    "url": "/deep-link-generator/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+    revision: "9780bde87bceec579eaf16bddad47615",
+    url: "/static/media/roboto-vietnamese-400-italic.9780bde8.woff2",
   },
   {
-    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
-    "url": "/deep-link-generator/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
-  }
+    revision: "a8be5b46d06bb541b0968196ee5e6bb8",
+    url: "/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2",
+  },
 ]);
